54a1144cde06eb870ab6295ea99ca1e9ed91c6ec
Mel Michael <mel.michael@andela.com>
2018-09-06 10:44:42 +0100

    Implement the listing of created stations

---
+++ src/app/mock-stations.ts
@@ -0,0 +1,58 @@
+import { Stations } from './station';
+
+export const STATIONS: Stations[] = [
+  {
+    'id': 1,
+    'name': 'station-blue',
+    'slots': [
+      {
+        'id': 11,
+        'name': 'st-11',
+        'is_free': true,
+        'busId': ''
+      },
+      {
+        'id': 12,
+        'name': 'st-12',
+        'is_free': true,
+        'busId': ''
+      },
+      {
+        'id': 13,
+        'name': 'st-13',
+        'is_free': true,
+        'busId': ''
+      },
+      {
+        'id': 14,
+        'name': 'st-14',
+        'is_free': true,
+        'busId': ''
+      }
+    ]
+  },
+  {
+    'id': 2,
+    'name': 'station-green',
+    'slots': [
+      {
+        'id': 21,
+        'name': 'st-21',
+        'is_free': true,
+        'busId': ''
+      },
+      {
+        'id': 22,
+        'name': 'st-22',
+        'is_free': true,
+        'busId': ''
+      },
+      {
+        'id': 23,
+        'name': 'st-23',
+        'is_free': true,
+        'busId': ''
+      }
+    ]
+  }
+]
---
+++ src/app/station.ts
@@ -0,0 +1,5 @@
+export class Stations {
+  id: number
+  name: string
+  slots: Array<Object>
+}
