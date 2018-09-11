ab3ac4103126db3bcca20da24f9c34b9c21e5d0a
Mel Michael <mel.michael@andela.com>
2018-09-11 18:08:42 +0100

    implement edit bus ui and functionality

--- src/app/buses/buses.component.ts
+++ src/app/buses/buses.component.ts
@@ -12,18 +12,20 @@ import { DataService } from '../data.service';
 export class BusesComponent implements OnInit {
   buses;
   stations;
-  temp;
-  busForm =  new FormGroup({
+  tempBusData;
+  busForm = new FormGroup({
     name: new FormControl(''),
     type: new FormControl(''),
     station: new FormControl('')
   });
-  busTypes = [ 'regular', 'doubledecker'];
+  busTypes = ['regular', 'doubledecker'];
   isBusSlotFree = true;
   showAddForm = false;
   addFormText = 'Add Bus';
+  showEditForm = false;
+  tempEditData;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService) {}
 
   addBus(): void {
     this.isBusSlotFree = true;
@@ -46,30 +48,84 @@ export class BusesComponent implements OnInit {
     const newSlot = station.slots.find(slot => !slot.busId);
     newSlot.busId = randNum;
     station.availableSlots--;
-    station.slots = station.slots.map((item) => {
+    station.slots = station.slots.map(item => {
       if (item.id === newSlot.id) { return newSlot; }
       return item;
     });
 
     this.dataService.createBus(busData, station)
-      .subscribe(() => this.getBuses(),
-        error => console.log('Error:', error)
-      );
+      .subscribe(() => this.getBuses(), error => console.error(error));
+  }
+
+  editBus() {
+    const { name, type, station } = this.busForm.value;
+    const { id, plateNumber, stationId } = this.tempEditData;
+    const editedBusData = {
+      id,
+      name,
+      type,
+      plateNumber,
+      stationId: station.id
+    };
+
+    if (station.id === stationId) {
+      return this.dataService.updateBus(editedBusData)
+        .subscribe(() => {
+          this.getBuses();
+          this.getBusStations();
+          this.closeEditForm();
+        }, error => console.error(error))
+    }
+
+    if (!station.availableSlots) {
+      this.isBusSlotFree = false;
+      return null;
+    }
+
+    const newSlot = station.slots.find(slot => !slot.busId);
+    station.availableSlots--;
+    station.slots = station.slots.map(item => {
+      if (item.id === newSlot.id) {
+        return newSlot;
+      }
+      return item;
+    });
+
+    this.dataService.updateStation(station)
+      .subscribe(() => {
+        this.getBuses();
+        this.getBusStations();
+        this.closeEditForm();
+      }, error => console.log(error));
   }
 
   showForm() {
     this.showAddForm = !this.showAddForm;
-    this.addFormText = this.showAddForm ? 'Close Bus' : 'Add Bus';
+    this.addFormText = this.showAddForm ? 'Close Station' : 'Add Station';
+  }
+
+  editForm(data) {
+    console.log(data);
+    this.showEditForm = true;
+    this.busForm.setValue({
+      name: data.name,
+      type: data.type,
+      station: { id: data.stationId, name: data.stationName }
+    });
+    this.tempEditData = data;
+  }
+
+  closeEditForm() {
+    this.showEditForm = false;
   }
 
   getBuses(): void {
     this.dataService.getBuses()
       .subscribe(data => {
         this.buses = data;
-        this.temp = data;
-      },
-        error => console.log('Error:', error)
-      );
+        this.tempBusData = data;
+      }, error => console.error(error)
+    );
   }
 
   getBusStations(): void {
@@ -82,13 +138,12 @@ export class BusesComponent implements OnInit {
           return bus;
         });
       },
-        error => console.log('Error:', error)
-      );
+      error => console.error(error)
+    );
   }
 
   updateBus(data): void {
-    this.dataService.updateBus(data)
-      .subscribe(() => this.getBuses());
+    this.dataService.updateBus(data).subscribe(() => this.getBuses());
   }
 
   deleteBus(busData): void {
@@ -105,22 +160,24 @@ export class BusesComponent implements OnInit {
 
   filterBy(value, props) {
     if (value === 'all' || !value) {
-      this.buses = this.temp;
+      this.buses = this.tempBusData;
       return null;
     }
 
     if (props === 'station') {
-      this.buses = this.temp.filter(bus => bus['stationId'] === parseInt(value, 10));
+      this.buses = this.tempBusData.filter(
+        bus => bus['stationId'] === parseInt(value, 10)
+      );
       return null;
     }
 
-    this.buses = this.temp.filter(bus => bus[props].toLowerCase().includes(value.toLowerCase()));
+    this.buses = this.tempBusData.filter(bus =>
+      bus[props].toLowerCase().includes(value.toLowerCase())
+    );
   }
 
   ngOnInit() {
     this.getBuses();
     this.getBusStations();
   }
-
-
 }
